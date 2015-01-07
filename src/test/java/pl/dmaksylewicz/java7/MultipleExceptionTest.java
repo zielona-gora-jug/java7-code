645d6b862689219c85308b18b48253fb26893107
dmaksylewicz <dawid.maksylewicz@gmail.com>
2015-01-07 23:30:54 +0100

    added impl + test for handling multiple exceptions

--- src/test/java/pl/dmaksylewicz/java7/MultipleExceptionTest.java
+++ src/test/java/pl/dmaksylewicz/java7/MultipleExceptionTest.java
@@ -1,5 +1,22 @@
 package pl.dmaksylewicz.java7;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import pl.dmaksylewicz.java7.MultipleException.FirstException;
+
 public class MultipleExceptionTest {
 
+	private MultipleException ex;
+
+	@Before
+	public void setUp() {
+		this.ex = new MultipleException();
+	}
+
+	@Test(expected = FirstException.class)
+	public void test_t() throws Exception {
+		int value = 1;
+		ex.throwMultipleException(value);
+	}
 }
